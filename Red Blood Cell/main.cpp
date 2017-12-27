3e6e872a9118fee2544ffb294cd0b280d50ba930
ali <a.farnudi@physics.sharif.ir>
2017-12-27 12:20:15 +0330

    Initial Commit

---
+++ Red Blood Cell/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Red Blood Cell
+//
+//  Created by Ali Farnudi on 27/12/2017.
+//  Copyright © 2017 Ali Farnudi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
